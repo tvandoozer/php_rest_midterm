d110e343daa6cac2a8cd08d3f82fdadfea656d8f
Travis <travis@vandoozer.com>
2024-03-14 18:10:21 -0600

    added index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>php-rest-midterm</title>
+  <style>
+    body {
+      background-color: #7395AE;
+      color: whitesmoke;
+    }
+  </style>
+</head>
+<body>
+  <h1>Midterm REST API</h1>
+</body>
+</html>
